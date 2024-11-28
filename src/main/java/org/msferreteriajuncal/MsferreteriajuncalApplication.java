058c1fde3f6feea57001321e218dd3b76b98ea63
linda paola carreno <lindapaolacarreno@gmail.com>
2024-11-27 23:31:10 -0500

    first commit

---
+++ src/main/java/org/msferreteriajuncal/MsferreteriajuncalApplication.java
@@ -0,0 +1,13 @@
+package org.msferreteriajuncal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsferreteriajuncalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsferreteriajuncalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/msferreteriajuncal/MsferreteriajuncalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msferreteriajuncal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsferreteriajuncalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
